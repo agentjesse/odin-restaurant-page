5cf45ca2c8a9f3b0c22183c791a059243f3f15aa
jesse <31576321+agentjesse@users.noreply.github.com>
2024-03-07 22:14:29 -0500

    Added some nav elements

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './styles.css'
 // module imports, from named and default
 //import { functionOne,functionTwo } from './myModule';
 
-//add elements. these will not be global variables since using modules!
+//javascript dom elements. these will not be global variables since using modules!
+//select elements
 const bodyElem = document.querySelector('body');
-bodyElem.appendChild(document.createElement('button'));
+//make elements
+const header = document.createElement('header');
+const nav = document.createElement('nav');
+const homeTabBtn = document.createElement('button');
+const menuTabBtn = document.createElement('button');
+const contactTabBtn = document.createElement('button');
+//append elements
+nav.append(homeTabBtn,menuTabBtn,contactTabBtn);
+header.append(nav);
+bodyElem.append(header);
