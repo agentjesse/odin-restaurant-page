564bd390f35fbff9bba89e8797ed6f3960b98e0b
jesse <31576321+agentjesse@users.noreply.github.com>
2024-03-07 22:14:29 -0500

    added package.json npm script: startQuietly
    also tested a button element addition

--- src/script.js
+++ src/script.js
@@ -4,4 +4,6 @@ import './styles.css'
 // module imports, from named and default
 //import { functionOne,functionTwo } from './myModule';
 
-// console.log('hi')
+//add elements. these will not be global variables since using modules!
+const bodyElem = document.querySelector('body');
+bodyElem.appendChild(document.createElement('button'));
